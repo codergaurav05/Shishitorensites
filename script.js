4cdf8b95d60908c2934d5d12e342baf1fd650204
codergaurav05 <senkufire485@gmail.com>
2024-12-31 09:04:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Function to navigate to a provided URL
+function navigateTo(url) {
+    window.location.href = url;
+}
+
+// Add event listeners to each button
+document.getElementById('button1').addEventListener('click', function() {
+    navigateTo('https://link1.com');
+});
+document.getElementById('button2').addEventListener('click', function() {
+    navigateTo('https://link2.com');
+});
+document.getElementById('button3').addEventListener('click', function() {
+    navigateTo('https://link3.com');
+});
+document.getElementById('button4').addEventListener('click', function() {
+    navigateTo('https://link4.com');
+});
+document.getElementById('button5').addEventListener('click', function() {
+    navigateTo('https://link5.com');
+});
+document.getElementById('button6').addEventListener('click', function() {
+    navigateTo('https://link6.com');
+});
+document.getElementById('button7').addEventListener('click', function() {
+    navigateTo('https://link7.com');
+});
